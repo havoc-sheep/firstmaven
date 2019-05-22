c92bdab3e9db2394bbdd6fa5496bdc19864d9392
havoc-sheep <21725170@zju.edu.cn>
2019-05-22 11:27:44 +0800

    first commit

---
+++ learnmaven/src/main/java/com/yang/test/Test01.java
@@ -0,0 +1,11 @@
+package com.yang.test;
+
+import com.alibaba.fastjson.JSON;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        JSON.toJSONString(new Object());
+        
+    }
+}
